a52b8bf2d298884b700d72383525f8a6a606ad3b
Paul Thomas <paulthomas2107@gmail.com>
2021-01-01 21:13:21 +0000

    Work in progress....

--- src/sidebaritem/sidebaritem.js
+++ src/sidebaritem/sidebaritem.js
@@ -7,11 +7,28 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import { removeHTMLTags } from "../helpers";
 
 class SidebarItemComponent extends React.Component {
-  constructor() {
-    super();
-  }
   render() {
-    return <div>Hello from SidebarItemComponent</div>;
+    const { _index, _note, classes, selectedNoteIndex } = this.props;
+
+    return (
+      <div key={_index}>
+        <ListItem
+          className={classes.listItem}
+          selected={selectedNoteIndex === _index}
+          alignItems="flex-start"
+        >
+          <div
+            className={classes.textSection}
+            onClick={() => this.selectNote(_note, _index)}
+          >
+            <ListItemText
+              primary={_note.title}
+              secondary={removeHTMLTags(_note.body.substring(0, 30)) + "..."}
+            ></ListItemText>
+          </div>
+        </ListItem>
+      </div>
+    );
   }
 }
 
