0d03b3e67ae5d503a0f368e28a2d1c97778cde16
Paul Thomas <paulthomas2107@gmail.com>
2021-01-01 18:34:57 +0000

    More code...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import "./App.css";
 import React from "react";
+import "./App.css";
+import firebase from "firebase";
+import SidebarComponent from './sidebar/sidebar'
+import EditorComponent from './editor/editor'
+
 
 class App extends React.Component {
-  render() {
-    return <div>Hello Paul</div>;
+  constructor() {
+    super();
+    this.state = {
+      selectedNoteIndex: null,
+      selectedNote: null,
+      notes: null,
+    };
   }
+
+  render() {
+    return (
+      <div className="app.container">
+        <SidebarComponent
+          selectedNoteIndex={this.state.selectedNoteIndex}
+          notes={this.state.notes}></SidebarComponent>
+        <EditorComponent></EditorComponent>
+      </div>
+    );
+  }
+
+  componentDidMount = () => {
+    firebase
+      .firestore()
+      .collection("notes")
+      .onSnapshot((serverUpdate) => {
+        const notes = serverUpdate.docs.map((_doc) => {
+          const data = _doc.data();
+          data["id"] = _doc.id;
+          return data;
+        });
+        console.log(notes);
+        this.setState({ notes: notes });
+      });
+  };
 }
 
 export default App;
---
+++ src/sidebar/sidebar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import styles from "./styles";
+import List from "@material-ui/core/List";
+import { Divider, Button } from "@material-ui/core";
+import SidebarItemComponent from "../sidebaritem/sidebaritem";
+
+class SidebarComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      addingNote: false,
+      title: null,
+    };
+  }
+
+  render() {
+    const { notes, classes, selectedNoteIndex } = this.props;
+
+    return (
+      <div className={classes.sidebarContainer}>
+        <Button onClick={this.newNoteBtnClick} className={classes.newNoteBtn}>
+          New Note
+        </Button>
+      </div>
+    );
+  }
+
+  newNoteBtnClick = () => {
+    console.log("New Note BTN Clickec");
+  };
+}
+
+export default withStyles(styles)(SidebarComponent);
